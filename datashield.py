5341a852769cdb78af1509d50e3ebc91eb08b87c
drdineshpatil9850 <drdineshpatil9850@gmail.com>
2026-06-15 07:35:02 -0700

    Update datashield.py

--- datashield.py
+++ datashield.py
@@ -264,7 +264,7 @@ if file is not None:
                         st.dataframe(data.head())
 
         elif file.name.endswith(".xlsx"):
-                data = pd.read_excel(file, ENGINE="openpyxl")
+                data = pd.read_excel(file, engine="openpyxl")
 
         columns = data.columns.tolist()
                         
